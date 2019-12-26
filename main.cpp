eb31f6c7284c2d5b09bb790867c0a05785f6d312
ironsmasher <55234253+ironsmasher@users.noreply.github.com>
2019-12-26 03:17:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,48 +3,37 @@
 // |E| = num_e
 #include <fstream>
 
-bool used[100]; // èñïîëüçóåòñÿ â ôóíêöèè ïðîâåðêè ñâÿçíîñòè
-int num_v; // êîëè÷åñòâî âåðøèí
+bool used[100]; 
+int num_v; 
 int **g;
 
-void dfs(int v) // ïðîâåðêà íà ñâÿçíîñòü
+void dfs(int v) 
 {
-	if (used[v] == true) // åñëè âåðøèíà èñïîëüçîâàíà( ìû â íåé áûëè) òî âûõîäèì èç ýòîãî âûçîâà
+	if (used[v] == true) 
 		return;
-	used[v] = true; // âåðøèíà èñïîëüçîâàíà
-	for (int i = 0; i < num_v; i++) // ñìîòðèì âñåõ ñîñåäåé òåêóùåé âåðøèíû
-		if (g[v][i] == 1) // ==1 åñëè åñòü ðåáðî â âåðøèíó i
-			dfs(i); //ðåêóðñèíûé âûçîâ îò íîâîé âåðøèíû
+	used[v] = true; 
+	for (int i = 0; i < num_v; i++) 
+		if (g[v][i] == 1)
+			dfs(i); 
 }
 
 int main()
 {
-	/*std::cout << "Input edges in format:"
-		<< std::endl << "First line: number of edges"
-		<< std::endl << "Second line: number of tops"
-		<< std::endl << "Next lines: pair (Vi, Vj) - mean connection Vi with Vj"
-		<< std::endl << "Example" 
-		<< std::endl << "3" << std::endl << "4" << std::endl <<  "1 2" << std::endl << "4 3" << std::endl << "1 2"
-		<< std::endl << "Lets start!" << std::endl;*/
-	
-	int num_e; // êîëè÷åñòâî ðåáåð
+	int num_e;
 
 	std::ifstream fin("input.txt");
 	std::ofstream fout("output.txt");
 	fin >> num_e; 
 	fin >> num_v;
 
-	// âûäåëåíèå ïàìÿòè ïîä ìàòðèöó ñìåæíîñòè
 	g = new int* [num_v]; 
 	for (int i = 0; i < num_v; i++)
 		g[i] = new int[num_v];
 	
-	// çàíóëåíèå ìàòðèöû
 	for (int i = 0; i < num_v; i++)
 		for (int j = 0; j < num_v; j++)
 			g[i][j] = 0;
 
-	// ââîä ãðàôà
 	for (int i = 0; i < num_e; i++)
 	{
 		int v1, v2;
@@ -54,7 +43,6 @@ int main()
 		g[v2][v1] = 1;
 	}
 
-	// âûâîä ìàòðèöû ñìåæíîñòè
 	for (int i = 0; i < num_v; i++)
 	{
 		for (int j = 0; j < num_v; j++)
@@ -62,14 +50,12 @@ int main()
 		fout << std::endl;
 	}
 
-	// âûçîâ ôóíêöèè ñâÿçíîñòè
 	dfs(0);
 
-	// ïðîâåðêà íà ñâÿçíîñòü
 	int i;
 	for (i = 0; i < num_v; i++)
 		if (used[i] == false)
-			break; // çíà÷èò íå âñå âåðøèíû ïðîéäåíû, çíà÷èò íå ñâÿçíûé ãðàô è i < num_v
+			break;
 	if (i >= num_v)
 		fout << "Graph svyazan" << std::endl;
 	else
@@ -89,11 +75,9 @@ int main()
 	system("graph.png");
 	system("pause");
 
-	// î÷èùåíèå ïàìÿòè
 	for (int i = 0; i < num_v; i++)
 		delete[]g[i];
 	delete[]g;
-	// çàêðûòèå ôàéëîâ
 	fin.close();
 	fout.close();
 
